6a534ddbfd2c8c1d5cd3ef81ef3c608e0ac2acee
Mir-Md-Reduan <mhoqu091050@bsee.uiu.ac.bd>
2021-10-31 21:36:59 +0600

    admin,myBooking, Add Touris Spot addded

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -4,19 +4,20 @@ import { useForm } from "react-hook-form";
 import axios from 'axios';
 import useAuth from '../../../Hooks/useAuth';
 import './Booking.css'
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const Booking = () => {
     const { bookingId } = useParams();
-    const { user } = useAuth();
+    const { user, isLoading } = useAuth();
     const [dataF, setDataF] = useState([]);
     useEffect(() => {
         fetch('https://grim-asylum-43912.herokuapp.com/places')
             .then(res => res.json())
             .then(dataF => setDataF(dataF))
-    }, []);
+    }, [isLoading]);
 
-    const ExactData = dataF.filter(dat => dat._id.toString === bookingId);
-    console.log(ExactData);
+    const ExactData = dataF.filter(dat => dat._id === bookingId);
+    // console.log(ExactData[0].name);
 
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
@@ -33,20 +34,38 @@ const Booking = () => {
     return (
         <div className="my-5 ">
             <h1>Booking Details </h1>
-            <p>
-                {/* Name : {ExactData?.name} <br />
-                price : {ExactData?.cost} <br />
-                description : {ExactData?.description} */}
-            </p>
-            <div className="booking-details">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("name", { required: true, maxLength: 20 })} placeholder="name" />
-                    <textarea {...register("description")} placeholder="description" />
-                    <input type="number" {...register("price")} placeholder="price" />
-                    <input {...register("img")} placeholder="imag url" />
-                    <input type="submit" />
-                </form>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="left-side">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={ExactData[0]?.img} />
+                                <Card.Body>
+                                    <Card.Title>Tour Spot Name : {ExactData[0]?.name}</Card.Title>
+                                    <Card.Text>
+                                        {ExactData[0]?.description}
+                                    </Card.Text>
+                                    <h3>Tour price per Person : ${ExactData[0]?.price} </h3>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="right-side">
+                            <div className="booking-details">
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <input {...register("name", { required: true, maxLength: 20 })} placeholder="name" />
+                                    <textarea {...register("description")} placeholder="description" />
+                                    <input type="number" {...register("price")} placeholder="price" />
+                                    <input {...register("img")} placeholder="imag url" />
+                                    <input type="submit" />
+                                </form>
+                            </div>
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Home/Shared/Admin/Admin.js
+++ src/Pages/Home/Shared/Admin/Admin.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const Admin = () => {
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        axios.post('https://grim-asylum-43912.herokuapp.com/admin', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert("added Successfully");
+                    reset();
+                }
+            })
+    };
     return (
         <div>
-            <h2>This is admin</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="left-side">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={ExactData[0]?.img} />
+                                <Card.Body>
+                                    <Card.Title>Tour Spot Name : {ExactData[0]?.name}</Card.Title>
+                                    <Card.Text>
+                                        {ExactData[0]?.description}
+                                    </Card.Text>
+                                    <h3>Tour price per Person : ${ExactData[0]?.price} </h3>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="right-side">
+                            <div className="booking-details">
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <input {...register("name", { required: true, maxLength: 20 })} placeholder="name" />
+                                    <textarea {...register("description")} placeholder="description" />
+                                    <input type="number" {...register("price")} placeholder="price" />
+                                    <input {...register("img")} placeholder="imag url" />
+                                    <input type="submit" />
+                                </form>
+                            </div>
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
